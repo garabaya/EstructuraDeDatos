4bd3baf4cb5704eba4821038effd6c3b87828030
Ruben Garabaya <rubengarabaya@gmail.com>
2017-12-29 16:24:03 +0100

    Clases Abb y NodeList

---
+++ src/arbolBinarioBusqueda/Abb.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package arbolBinarioBusqueda;
+
+/**
+ * @author Ruben Garabaya Arenas
+ *
+ */
+public class Abb {
+	private Node root;
+	//El árbol se crea vacío
+	public Abb() {
+		this.root=null;
+	}
+	//lanzar error si ya existe, capturarlo y devolver false
+	public boolean addNode(Node nodo) {
+		Node posicion = this.root;
+		//TODO
+		return true;
+	}
+	public NodeList<Object> pre(){
+		NodeList<Object> lista = new NodeList<Object>();
+		//TODO
+		return lista;		
+	}
+	public NodeList<Object> in(){
+		NodeList<Object> lista = new NodeList<Object>();
+		//TODO
+		return lista;		
+	}
+	public NodeList<Object> post(){
+		NodeList<Object> lista = new NodeList<Object>();
+		//TODO
+		return lista;		
+	}
+
+}
---
+++ src/arbolBinarioBusqueda/NodeList.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package arbolBinarioBusqueda;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ruben Garabaya Arenas
+ *
+ */
+public class NodeList<Object> extends ArrayList<Object>{
+
+	public void imprimir() {
+		//TODO
+	}
+
+}
